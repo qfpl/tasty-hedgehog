64068d1fe03ffc2c9f4508fe072cf7015e05ab5b
Isaac Elliott <isaace71295@gmail.com>
2017-08-18 10:50:15 +1000

    Simplified shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,2 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-
-let
-  inherit (nixpkgs) pkgs;
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  drv = haskellPackages.callPackage ./tasty-hedgehog.nix {};
-
-in if pkgs.lib.inNixShell then drv.env else drv
+(import ./default.nix { inherit nixpkgs compiler; }).env
