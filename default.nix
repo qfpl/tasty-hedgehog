9691893933200a35885aa8c44fdf3d1b5a3657b6
Dave Laing <dave.laing.80@gmail.com>
2017-08-21 11:04:17 +1000

    Grabs `hedgehog` from GitHub.
    At the moment `hedgehog-0.5` isn't in the stable nixos channel, so
    this is a stopgap until that rolls forward.
    
    It is using the version of `hedgehog` with the `0.5` tag in GitHub,
    so hopefully we're getting the same as what we'd get from Hackage.

--- default.nix
+++ default.nix
@@ -6,4 +6,28 @@ let
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-in haskellPackages.callPackage ./tasty-hedgehog.nix {}
+  hedgehog = pkgs.stdenv.mkDerivation {
+    name = "hedgehog-0.5";
+    src = pkgs.fetchFromGitHub {
+      owner = "hedgehogqa";
+      repo = "haskell-hedgehog";
+      rev = "2591bd243751a21f0d7bf57fd54f1c30d07aa270";
+      sha256 = "0dxkh97070kvfcsr5snj27fs2ckalsvqp14mwyjp4dywawdqs6n7";
+    };
+    phases = ["installPhase"];
+    installPhase = ''
+      mkdir $out
+      cp -r $src/hedgehog/* $out/
+      rm $out/LICENSE
+      cp $src/LICENSE $out/
+    '';
+  };
+
+  modifiedHaskellPackages = haskellPackages.override {
+    overrides = self: super: {
+      hedgehog = self.callCabal2nix "hedgehog" hedgehog {};
+    };
+  };
+
+in 
+  modifiedHaskellPackages.callPackage ./tasty-hedgehog.nix {}
