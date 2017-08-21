bd417b8eb3d465ba2fa0c71b232d9a3e2fd0858e
Dave Laing <dave.laing.80@gmail.com>
2017-08-21 11:17:59 +1000

    Adds hydra support

---
+++ ci/jobsets.nix
@@ -0,0 +1,27 @@
+{ nixpkgs, declInput }: let pkgs = import nixpkgs {}; in {
+  jobsets = pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toXML declInput}
+    EOF
+    cat > $out <<EOF
+    {
+        "tasty-hedgehog": {
+            "enabled": 1,
+            "hidden": false,
+            "description": "tasty-hedgehog",
+            "nixexprinput": "tasty-hedgehog",
+            "nixexprpath": "ci/ci.nix",
+            "checkinterval": 300,
+            "schedulingshares": 1,
+            "enableemail": false,
+            "emailoverride": "",
+            "keepnr": 5,
+            "inputs": {
+                "tasty-hedgehog": { "type": "git", "value": "https://github.com/qfpl/tasty-hedgehog", "emailresponsible": false },
+                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-17.03", "emailresponsible": false }
+            }
+        }
+    }
+    EOF
+  '';
+}
