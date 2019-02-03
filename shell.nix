3cc4859f7ad42c4409a0b169fe8d9b29138eeb7c
Dave Laing <dave.laing.80@gmail.com>
2019-02-04 09:40:44 +1000

    Brings cabal-install into play when using nix-shell.
    This helps with testing out alternative versions of nixpkgs, so
    that the version of Cabal you have is in sync with that and the
    rest of your libraries and tools.

--- shell.nix
+++ shell.nix
@@ -1,2 +1,7 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+let
+  inherit (nixpkgs) pkgs;
+  drv = import ./default.nix { inherit nixpkgs compiler; };
+  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [ pkgs.cabal-install ];
+in
+  if pkgs.lib.inNixShell then drvWithTools.env else drvWithTools
